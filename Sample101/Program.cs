fab3e056de43f71ac9b79c00bfa97e6f66b41e34
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-03-16 05:42:52 +0900

    :sparkles: Consoleへの出力方法

--- Sample101/Program.cs
+++ Sample101/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+/* Console への出力
+Console.Write は改行無しで出力
+Console.WriteLine は改行して出力
+*/
+Console.Write("H");
+Console.Write("e");
+Console.Write("l");
+Console.Write("l");
+Console.Write("o");
+Console.Write(",");
+Console.WriteLine("World!");
+//上記出力結果: Hello,World!
---
+++ Sample201/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Sample201;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("{0} + {1} = {2}", 5, 5,1);
+
+        // Console.WriteLine("Hello, World!");
+        // Console.WriteLine(10+3);
+        // Console.WriteLine(10-3);
+        // Console.WriteLine(10/3);
+        // Console.WriteLine(10%3);
+    }
+}

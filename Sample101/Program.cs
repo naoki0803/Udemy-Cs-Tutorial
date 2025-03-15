04a5456113281a4b58f5f8aaf27c7caee5946d5a
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-03-16 05:44:06 +0900

    :sparkles: 変数宣言方法

--- Sample101/Program.cs
+++ Sample101/Program.cs
@@ -13,3 +13,17 @@ Console.Write("o");
 Console.Write(",");
 Console.WriteLine("World!");
 //上記出力結果: Hello,World!
+
+
+/* 変数宣言
+変数宣言は type 変数名 = 値; と記述をする
+*/
+int a = 3;
+int b = 10;
+double avg = (a + b) / 2.0; //2.0 と表現しないと整数として値を返してしまう。。。
+string firstName = "鈴木", lastName = "太郎";   // 1つのTypeに複数
+
+Console.WriteLine(a);
+Console.WriteLine(firstName, lastName);
+Console.WriteLine(avg);
+

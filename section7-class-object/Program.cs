4fff57d0752f797fc1c7f45853d7030de293fe82
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-04-01 06:34:48 +0900

    :sparkles: ガーベジコレクターの実装

--- section7-class-object/Program.cs
+++ section7-class-object/Program.cs
@@ -1,68 +1,71 @@
 ﻿using section7_class_object; // section7_class_object クラスが定義されている名前空間を追加
 
-// using static Calc;
-// using static Person;
+// // using static Calc;
+// // using static Person;
 
-// インスタンス化
-Person taro = new Person();
+// // インスタンス化
+// Person taro = new Person();
 
-// class 内で初期化された値が表示される
-Console.WriteLine(taro.name);   // 初期値太郎
-Console.WriteLine(taro.age);    // 0
+// // class 内で初期化された値が表示される
+// Console.WriteLine(taro.name);   // 初期値太郎
+// Console.WriteLine(taro.age);    // 0
 
-// 値を代入
-taro.name = "太郎";
-taro.age = 39;
+// // 値を代入
+// taro.name = "太郎";
+// taro.age = 39;
 
-// 代入した値が表示される
-Console.WriteLine(taro.name);   // 太郎
-Console.WriteLine(taro.age);    // 39
+// // 代入した値が表示される
+// Console.WriteLine(taro.name);   // 太郎
+// Console.WriteLine(taro.age);    // 39
 
-// class内で定義したメソッドで値を代入する
-taro.SetAgeAndName("鈴木一郎", 40);
-Console.WriteLine(taro.name);   // 鈴木一郎
-Console.WriteLine(taro.age);    // 40
+// // class内で定義したメソッドで値を代入する
+// taro.SetAgeAndName("鈴木一郎", 40);
+// Console.WriteLine(taro.name);   // 鈴木一郎
+// Console.WriteLine(taro.age);    // 40
 
-// class内で定義したメソッドで結果が表示される
-taro.ShowAgeAndName();   //名前:鈴木一郎 年齢:40
-
-
-Calc cal = new Calc();
-Console.WriteLine(cal.Add(1, 2));
-Console.WriteLine(cal.Add(1, 2, 3));
-// 4つ目の引数は存在しないため、コンパイルエラーになります。
-// Console.WriteLine(cal.Add(1, 2, 3, 4)); 
+// // class内で定義したメソッドで結果が表示される
+// taro.ShowAgeAndName();   //名前:鈴木一郎 年齢:40
 
 
-// コンストラクタ関数でkondをインスタンス化
-AccessModifiersPerson kondo = new AccessModifiersPerson("近藤", 20);
+// Calc cal = new Calc();
+// Console.WriteLine(cal.Add(1, 2));
+// Console.WriteLine(cal.Add(1, 2, 3));
+// // 4つ目の引数は存在しないため、コンパイルエラーになります。
+// // Console.WriteLine(cal.Add(1, 2, 3, 4)); 
 
-// Nameプロパティの参照(getter)
-Console.WriteLine(kondo.Name);
-// Nameプロパティの変更(setter)
-kondo.Name = "kondo";
 
-Console.WriteLine(kondo.Name);
+// // コンストラクタ関数でkondをインスタンス化
+// AccessModifiersPerson kondo = new AccessModifiersPerson("近藤", 20);
 
-// コンストラクタ関数でAutoPropertyをインスタンス化
-AutoImplementProperty AutoProperty = new AutoImplementProperty("自動プロパティ君", 44);
+// // Nameプロパティの参照(getter)
+// Console.WriteLine(kondo.Name);
+// // Nameプロパティの変更(setter)
+// kondo.Name = "kondo";
 
-// Nameプロパティの参照(getter)
-Console.WriteLine(AutoProperty.Name);
+// Console.WriteLine(kondo.Name);
 
-// AutoImplementPropertyの Name プロパティは、 private set; get; としており、変更ができない
-// AutoProperty.Name = "kondo";  // コンパイルエラーになる為コメントアウト
+// // コンストラクタ関数でAutoPropertyをインスタンス化
+// AutoImplementProperty AutoProperty = new AutoImplementProperty("自動プロパティ君", 44);
 
-Console.WriteLine(AutoProperty.Name);
+// // Nameプロパティの参照(getter)
+// Console.WriteLine(AutoProperty.Name);
 
-// 練習問題1
-Practice1Vector2D v1 = new Practice1Vector2D(1, 1);
-Practice1Vector2D v2 = new Practice1Vector2D(1, -1);
-v1.Add(v2);
-Console.WriteLine($"{v1.X}, {v1.Y}");
-v1.Sub(v2);
-Console.WriteLine($"{v1.X}, {v1.Y}");
-v1.Mul(2.0);
-Console.WriteLine($"{v1.X}, {v1.Y}");
-double resolute = v1.DotProduct(v2);
-Console.WriteLine(resolute);
+// // AutoImplementPropertyの Name プロパティは、 private set; get; としており、変更ができない
+// // AutoProperty.Name = "kondo";  // コンパイルエラーになる為コメントアウト
+
+// Console.WriteLine(AutoProperty.Name);
+
+// // 練習問題1
+// Practice1Vector2D v1 = new Practice1Vector2D(1, 1);
+// Practice1Vector2D v2 = new Practice1Vector2D(1, -1);
+// v1.Add(v2);
+// Console.WriteLine($"{v1.X}, {v1.Y}");
+// v1.Sub(v2);
+// Console.WriteLine($"{v1.X}, {v1.Y}");
+// v1.Mul(2.0);
+// Console.WriteLine($"{v1.X}, {v1.Y}");
+// double resolute = v1.DotProduct(v2);
+// Console.WriteLine(resolute);
+
+GarbageCollector gc = new GarbageCollector();
+gc.RunDemo();

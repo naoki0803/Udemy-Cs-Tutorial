c346839a072376b107cf363acaa34651f7325eb7
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-04-05 20:18:24 +0900

    :sparkles: ObjectClass

--- section8-inheritance/Program.cs
+++ section8-inheritance/Program.cs
@@ -19,3 +19,32 @@ Console.WriteLine($"子クラスDiv:{childCalc.Div()}");
 ParentCalc OverlordCalc = new ChildCalc(30, 20);
 Console.WriteLine($"子クラスAdd:{childCalc.Add()}");
 Console.WriteLine($"overrideしたAdd:{OverlordCalc.Add()}");
+
+
+/*
+    オブジェクトクラス
+    全てのクラスはObjectClassを継承しており、ObjectClassのメソッドを使用できる
+*/
+Program s = new Program();
+Program s2 = new Program();
+
+// ObjectClassの.ToString()メソッドを呼び出すと、オブジェクトの文字列表現を取得できる
+Console.WriteLine(s.ToString());
+
+// ObjectClassのEqualsメソッドを呼び出すと、オブジェクトの等価性を確認できる
+Console.WriteLine(s.Equals(s2));
+Console.WriteLine(s.Equals(s));
+
+// ObjectClassの.GetHashCode()メソッドを呼び出すと、オブジェクトのハッシュコードを取得できる
+Console.WriteLine(s.GetHashCode());
+Console.WriteLine(s2.GetHashCode());
+
+// ObjectClassの型
+Console.WriteLine(s.GetType());
+
+// ObjectClassの.GetType()メソッドを呼び出すと、オブジェクトの型を取得できる
+Console.WriteLine(s.GetType());
+
+// ObjectClassの.MemberwiseClone()メソッドを呼び出すと、オブジェクトの浅いコピーを作成できる
+Program s3 = (Program)s.MemberwiseClone();
+Console.WriteLine(s3.ToString());

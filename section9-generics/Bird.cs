97693925760a45c8b25f9cd68010629e0bd2c588
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-04-07 06:16:47 +0900

    :sparkles: 抽象プロパティ

--- section9-generics/Bird.cs
+++ section9-generics/Bird.cs
@@ -5,7 +5,23 @@
 // abstract を付けると、抽象クラスとして実装される
 public abstract class Bird(string name)
 {
-    public string Name { get; } = name;
+    protected readonly string _name = name;
+    /* 
+        象プロパティ
+        派生クラスで override して必ず実装する必要がある
+    */
 
+    public abstract string Name { get; }
+
+    /*
+    以下のように = name; を記述すると、実装を記述することになりコンパイルエラーになる。
+    abstract をつけたプロパティは、派生クラスで override して必ず実装する必要がある。
+    */
+    // public abstract string Name { get; } = name;
+
+    /* 
+        抽象メソッド
+        派生クラスで override して必ず実装する必要がある
+    */
     public abstract void Sing();
 }
--- section9-generics/Sparrow.cs
+++ section9-generics/Sparrow.cs
@@ -4,7 +4,17 @@
 // public class Sparrow(string name)   //継承前の記述
 public class Sparrow() : Bird("すずめ")
 {
-    // public string Name { get; } = name;
+    public override string Name => _name;
+    /*
+        上記コードは以下をラムダ式で記述している    
+        public override string Name
+        {
+            get { return _name; }
+        }
+
+        また、自動実装プロパティでは、以下の記述は構文エラーとなる。
+        public override string Name { get; } = _name;
+    */
 
     // public void Sing()
     public override void Sing()

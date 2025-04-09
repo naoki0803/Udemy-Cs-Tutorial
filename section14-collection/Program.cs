72be54bfa6f288e90efead179e747d958e114da4
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-04-10 06:37:26 +0900

    :sparkles: collectionの定義

--- section14-collection/Program.cs
+++ section14-collection/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> list = new List<int>();
+
+list.Add(1);
+list.Add(2);
+list.Add(3);
+
+Console.WriteLine(list);
+
+list.Insert(0, 4);
+
+
+// list の様々な出力方法
+Console.WriteLine(string.Join("\n", list)); // 改行
+Console.WriteLine(string.Join(" ", list));  // 半角スペース
+foreach (var item in list)                  // foreach で愚直に
+{
+    Console.WriteLine(item);
+}

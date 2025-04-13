160b9b04cd0a4c60d73c5ec1629d8a7543c05bb6
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-04-13 12:10:11 +0900

    :sparkles: delegateの実装

--- section15-delegate/Program.cs
+++ section15-delegate/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// デリゲート
+delegate void Operation(int a, int b);
+
+// Calcクラス
+class Calc
+{
+    public void Sub(int a, int b)
+    {
+        Console.WriteLine($"CalcクラスのSubメソッドで実行した結果: {a}-{b}={a - b}");
+    }
+}
+
+class Program
+{
+    static void Add(int a, int b)
+    {
+        Console.WriteLine($"static メソッドの Add で実行した結果: {a}+{b}={a + b}");
+    }
+    static void Main(string[] args)
+    {
+        Calc c = new Calc();
+        Operation o1 = new Operation(Add);
+        Operation o2 = new Operation(c.Sub);
+
+        // デリゲートで設定したメソッドの呼び出し
+        o1(2, 1);
+        o2(2, 1);
+    }
+}

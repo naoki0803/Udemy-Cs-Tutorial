6cf46190f7a661ea53f4a610d662e2827fe2b64b
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-04-15 12:15:05 +0900

    :sparkles: C#における例外処理のサンプルコードを追加

---
+++ section15-exception/Program.cs
@@ -0,0 +1,43 @@
+﻿
+static int getNum(int idx)
+{
+    int[] numbers = { 1, 2, 3, 4, 0 };  //IndexOutOfRangeExceptionが発生する
+    // int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+    return numbers[idx];
+}
+
+static int Calc(int a, int b)
+{
+    return a / b;
+}
+
+try
+{
+
+    for (int i = 0; i <= 4; i++)
+    {
+        int a = getNum(i);
+        int b = 5;
+        Console.WriteLine($"{a}/{b}={Calc(a, b)}");
+
+    }
+    throw new IndexOutOfRangeException();
+    throw new DivideByZeroException();
+
+
+}
+
+catch (DivideByZeroException)
+{
+    Console.WriteLine("0除算が実行されました");
+}
+catch (IndexOutOfRangeException e)
+{
+    Console.WriteLine("配列の範囲外にアクセスしました");
+    // Console.WriteLine(e);
+}
+finally
+{
+    Console.WriteLine("処理を終了します");
+
+}
